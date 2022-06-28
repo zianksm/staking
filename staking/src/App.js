287dfe7810e316f8a58fda23dfd1f6f7a1065bb9
zian <mynameiszian@gmail.com>
2022-06-28 23:45:52 +0800

    finish building search stakes FE

--- staking/src/App.js
+++ staking/src/App.js
@@ -1,17 +1,68 @@
+import { Button, ButtonGroup } from "@mui/material";
 import "./App.css";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useEffect, useState } from "react";
 
-function App() {
+
+
+export default function App() {
+  
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#0971f1',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#ED7F68',
+      contrastText: '#fff',
+    },
+    black:{
+      main: "#000000"
+    },
+    white:{
+      main: "#FFFFFF"
+    }
+  },
+});
+
   return (
+    <ThemeProvider theme={theme}>
+      <NavBar>
+
+      </NavBar>
     <div className="App">
       <header className="App-header">
-        <box style={{ border: "1px solid black", padding:"15px", borderRadius: "10px", display: "flex", flexDirection: "row" }}>
-          <input placeholder="Public Address" style={{margin:"0 5px"}} />
+          <h3 id="staking-word">Search Stakes</h3>
+        <box className="container">
+          <input id="pub-adress-input" placeholder="Public Address" style={{margin:"0 5px"}} />
           <br />
-          <button>search</button>
+          <Button variant="text" color="neutral" >search</Button>
         </box>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+
+const NavBar = ()=> {
+
+
+  return (
+   <div className="Navbar">
+    <ThemeProvider>
+
+    <ButtonGroup variant="text">
+      <Button color="white">Stake</Button>
+    </ButtonGroup>
+    </ThemeProvider>
+   </div>
+  )
+}
+
+
+
