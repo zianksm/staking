ad8efdbfab604d2a1d90e30ec3d6bc7bfa95dbee
zian <mynameiszian@gmail.com>
2022-11-06 14:28:44 +0800

    remove unrelated things

--- API/src/contract_handlers/key_instance.rs
+++
@@ -1,8 +0,0 @@
-use std::str::FromStr;
-use web3::signing::SecretKey;
-
-pub fn get_prv_key(key: String)->SecretKey {
-    let key = SecretKey::from_str(&key).expect("error converting into secret key");
-
-    return key
-}
